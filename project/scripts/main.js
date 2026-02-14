63ca41278e599f7ab6e369a4fb074c2d3fdde673
veelustrous <vmalagu@byupathway.edu>
2026-02-15 00:39:16 +0100

    final changes

--- project/scripts/main.js
+++ project/scripts/main.js
@@ -70,3 +70,22 @@ if (form) {
     form.reset();
   });
 }
+// Collapsible FAQ
+const faqButtons = document.querySelectorAll(".faq-question");
+
+faqButtons.forEach(btn => {
+  btn.addEventListener("click", () => {
+    const answer = btn.nextElementSibling;
+    if (answer.style.display === "block") {
+      answer.style.display = "none";
+    } else {
+      answer.style.display = "block";
+    }
+  });
+});
+faqButtons.forEach(btn => {
+  btn.addEventListener("click", () => {
+    const answer = btn.nextElementSibling;
+    answer.classList.toggle("show");
+  });
+});
